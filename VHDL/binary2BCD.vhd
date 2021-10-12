7a513d0a4bf71bc4ae2f01679f669877e5aa06c3
beeeeep <theprotzect@gmail.com>
2021-10-13 00:50:34 +0200

    first commit

--- VHDL/binary2BCD.vhd
+++ VHDL/binary2BCD.vhd
@@ -24,53 +24,53 @@ architecture structural of binary2BCD is
     signal Binary_in_buff: std_logic_vector(WIDTH-1 downto 0);  
     signal Binary_in_buff_next: std_logic_vector(WIDTH-1 downto 0); 
 
-    signal  BCD_out_buff: std_logic_vector (9 downto 0);
-    signal  BCD_out_buff_next: std_logic_vector (9 downto 0);
+    signal  BCD_sreg: std_logic_vector (9 downto 0);
+    signal  BCD_sreg_next: std_logic_vector (9 downto 0);
 
     signal  counter: std_logic_vector (3 downto 0);
     signal  counter_next: std_logic_vector (3 downto 0);
     signal  count_done: std_logic;
 
-    signal  BCD_out_final_reg: std_logic_vector (9 downto 0);
-    signal  BCD_out_final_reg_next: std_logic_vector (9 downto 0);
+    signal  BCD_out_buffer: std_logic_vector (9 downto 0);
+    signal  BCD_out_buffer_next: std_logic_vector (9 downto 0);
     
     
 begin
 
-    Double_dabble_sequential: process(clk,counter) --converts bin to bcd, BCD_out_buff is the output signal
+    Double_dabble_sequential: process(clk,counter) --converts bin to bcd, BCD_sreg is the output signal
     begin
         if(rising_edge(clk)) then
             if reset='1' then             
-                BCD_out_buff<="0000000000";
+                BCD_sreg<="0000000000";
                 Binary_in_buff<="00000000";
             else           
                 Binary_in_buff<=Binary_in_buff_next;                            
-                BCD_out_buff<=BCD_out_buff_next;               
+                BCD_sreg<=BCD_sreg_next;               
             end if;
         end if;      
     end process;
 
-    Double_dabble_combinational: process(reset,binary_in,counter,Binary_in_buff,BCD_out_buff)--converts bin to bcd, BCD_out_buff is the output signal
+    Double_dabble_combinational: process(reset,binary_in,counter,Binary_in_buff,BCD_sreg)--converts bin to bcd, BCD_sreg is the output signal
     begin
         if reset='1' then  
-            BCD_out_buff_next<="0000000000";
+            BCD_sreg_next<="0000000000";
             Binary_in_buff_next<=binary_in;
         else
-            if(count_done<='0') then
+            if(count_done='0') then
                 Binary_in_buff_next(WIDTH-1 downto 1)<=Binary_in_buff(WIDTH-2 downto 0);
                 Binary_in_buff_next(0)<='0';
-               if(BCD_out_buff(3 downto 0)>="0101") then
-                        BCD_out_buff_next(9 downto 1)<=BCD_out_buff(8 downto 0)+"0011";
-                        BCD_out_buff_next(0)<=Binary_in_buff(WIDTH-1);
-              elsif (BCD_out_buff(7 downto 4)>="0101") then
-                        BCD_out_buff_next(9 downto 1)<=BCD_out_buff(8 downto 0)+"00110000";
-                        BCD_out_buff_next(0)<=Binary_in_buff(WIDTH-1);
+               if(BCD_sreg(3 downto 0)>="0101") then
+                        BCD_sreg_next(9 downto 1)<=BCD_sreg(8 downto 0)+"0011";
+                        BCD_sreg_next(0)<=Binary_in_buff(WIDTH-1);
+              elsif (BCD_sreg(7 downto 4)>="0101") then
+                        BCD_sreg_next(9 downto 1)<=BCD_sreg(8 downto 0)+"00110000";
+                        BCD_sreg_next(0)<=Binary_in_buff(WIDTH-1);
                else
-                        BCD_out_buff_next(9 downto 1)<=BCD_out_buff(8 downto 0);
-                        BCD_out_buff_next(0)<=Binary_in_buff(WIDTH-1);
+                        BCD_sreg_next(9 downto 1)<=BCD_sreg(8 downto 0);
+                        BCD_sreg_next(0)<=Binary_in_buff(WIDTH-1);
                end if;       
             else               
-                BCD_out_buff_next<="0000000000";     
+                BCD_sreg_next<="0000000000";     
                 Binary_in_buff_next<=binary_in;                
            end if;
         end if;
@@ -78,27 +78,28 @@ begin
 
 
 
-Output_buffer_sequential: process(clk,reset,BCD_out_final_reg_next)
+Output_buffer_sequential: process(clk,reset,BCD_out_buffer_next)
 begin
      if(rising_edge(clk)) then
        if reset='1' then
-          BCD_out_final_reg<="0000000000";
+          BCD_out_buffer<="0000000000";
        else
-          BCD_out_final_reg<= BCD_out_final_reg_next;            
+          BCD_out_buffer<= BCD_out_buffer_next;            
        end if;
    end if;      
 end process;
 
-Output_buffer_combinational: process(reset,count_done,BCD_out_final_reg)
+Output_buffer_combinational: process(reset,count_done,BCD_out_buffer)
    begin   
           if reset='1' then
-             BCD_out_final_reg_next<="0000000000";
-          elsif(count_done<='0') then 
-             BCD_out_final_reg_next<= BCD_out_final_reg;
+             BCD_out_buffer_next<="0000000000";
+          elsif(count_done='1') then 
+             
+             BCD_out_buffer_next<=BCD_sreg;
            else
-            BCD_out_final_reg_next<=BCD_out_buff;           
+            BCD_out_buffer_next<= BCD_out_buffer;        
           end if;
-          BCD_out<=BCD_out_final_reg; 
+          BCD_out<=BCD_out_buffer; 
 end process;
 
 
@@ -125,7 +126,7 @@ Counter_combinational: process(clk,reset,counter)
            counter_next<="0000";
            count_done<='1';        
          end if;
-         BCD_out<=BCD_out_final_reg; 
+         BCD_out<=BCD_out_buffer; 
 end process;
 
 

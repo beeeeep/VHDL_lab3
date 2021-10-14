937ecfd649d121ac54940087fcc0c9161dfc7bd4
beeeeep <theprotzect@gmail.com>
2021-10-14 12:15:17 +0200

    added seg_display

--- VHDL/7SegDriver.vhd
+++ VHDL/7SegDriver.vhd
@@ -3,22 +3,179 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity seven_seg_driver is
-   port ( clk           : in  std_logic;
-          reset         : in  std_logic;
-          BCD_digit     : in  std_logic_vector(9 downto 0);          
-          sign          : in  std_logic;
-          overflow      : in  std_logic;
-          DIGIT_ANODE   : out std_logic_vector(3 downto 0);
-          SEGMENT       : out std_logic_vector(6 downto 0)
+    port ( clk           : in  std_logic;
+         reset         : in  std_logic;
+         BCD_digit     : in  std_logic_vector(9 downto 0);
+         sign          : in  std_logic;
+         overflow      : in  std_logic;
+         DIGIT_ANODE   : out std_logic_vector(3 downto 0);
+         SEGMENT       : out std_logic_vector(7 downto 0)
         );
 end seven_seg_driver;
 
 architecture behavioral of seven_seg_driver is
 
--- SIGNAL DEFINITIONS HERE IF NEEDED
-
+    signal Seg_reg_0,Seg_reg_1,Seg_reg_2,Seg_reg_3:  std_logic_vector(6 downto 0);
+    signal counter,counter_next:unsigned(1 downto 0);
+    signal counter_delay,counter_delay_next:unsigned(9 downto 0);
 begin
 
--- DEVELOPE YOUR CODE HERE
 
+
+
+    --/*******************************************************
+    --/
+    --/       Binary to 7 Segment transformations
+    --/ 
+    --/*******************************************************
+
+    Bin_to_Seg_3: process(BCD_digit)
+    begin
+        case BCD_digit(3 downto 0) is
+            when "0000" =>
+                Seg_reg_3 <= "11000000";
+            when "0001" =>
+                Seg_reg_3 <= "11111001";
+            when "0010" =>
+                Seg_reg_3 <= "10100100";
+            when "0011" =>
+                Seg_reg_3 <= "10110000";
+            when "0100" =>
+                Seg_reg_3 <= "10011001";
+            when "0101" =>
+                Seg_reg_3 <= "10010010";
+            when "0110" =>
+                Seg_reg_3 <= "10000010";
+            when "0111" =>
+                Seg_reg_3 <= "11111000";
+            when "1000" =>
+                Seg_reg_3 <= "10000000";
+            when "1001" =>
+                Seg_reg_3 <= "10011000";
+            when "1111" =>
+                Seg_reg_3 <= "00000000";  --clear the display 
+            when others =>
+                Seg_reg_3 <= "10000110";
+        end case;
+    end process;
+
+    Bin_to_Seg_2: process(BCD_digit)
+    begin
+        case BCD_digit(7 downto 4) is
+            when "0000" =>
+                Seg_reg_2 <= "11000000";
+            when "0001" =>
+                Seg_reg_2 <= "11111001";
+            when "0010" =>
+                Seg_reg_2 <= "10100100";
+            when "0011" =>
+                Seg_reg_2 <= "10110000";
+            when "0100" =>
+                Seg_reg_2 <= "10011001";
+            when "0101" =>
+                Seg_reg_2 <= "10010010";
+            when "0110" =>
+                Seg_reg_2 <= "10000010";
+            when "0111" =>
+                Seg_reg_2 <= "11111000";
+            when "1000" =>
+                Seg_reg_2 <= "10000000";
+            when "1001" =>
+                Seg_reg_2 <= "10011000";
+            when "1111" =>
+                Seg_reg_2 <= "00000000";  --clear the display 
+            when others =>
+                Seg_reg_2 <= "10000110";
+        end case;
+    end process;
+
+    Bin_to_Seg_1: process(BCD_digit)
+    begin
+        case BCD_digit(8 downto 9) is
+            when "00" =>
+                Seg_reg_2 <= "11000000";
+            when "01" =>
+                Seg_reg_2 <= "11111001";
+            when "10" =>
+                Seg_reg_2 <= "10100100";
+            when "11" =>
+                Seg_reg_2 <= "10110000";
+            when others =>
+                Seg_reg_2 <= "10000110";
+        end case;
+    end process;
+
+    Sign_to_Seg_0:process(sign)
+    begin
+        if(sign='1') then
+            Seg_reg_0<="01000000";
+        else
+            Seg_reg_0<="11111111";
+        end if;
+    end process;
+
+    --/*******************************************************
+    --/
+    --/      7 Segment driver processes
+    --/ 
+    --/*******************************************************
+    Segment_scan_delay:   process(clk,reset,counter_delay,counter)
+    begin
+        if rising_edge(clk)then
+            if(reset='1') then
+                counter_delay_next<="0000000000";
+                counter_delay<="0000000000";
+            else
+                counter_delay<=counter_delay_next;
+            end if;
+        end if;
+        counter_delay_next<=counter_delay+1;
+        if( counter_delay="1111111111") then
+            counter_next <= counter + 1;
+        else
+            counter_next<=counter;
+        end if;
+    end process;
+
+    Seg_driver: process(counter,Seg_reg_0,Seg_reg_1,Seg_reg_2,Seg_reg_3) -- scans each 7-seg display with a shift register, changes the code to display, according to the register enabled
+    begin
+        if(overflow='1') then -- if overflow is detected, show dashes to all segments
+            DIGIT_ANODE <= "0000";
+            SEGMENT <= "01000000";
+        else
+            case counter is
+                when "00" =>
+                    if Seg_reg_0 = "00000000" then
+                        DIGIT_ANODE <= "1111";
+                    else
+                        DIGIT_ANODE <= "1110";
+                        SEGMENT <= Seg_reg_0;
+                    end if;
+                when "01" =>
+                    if Seg_reg_1 = "00000000" then
+                        DIGIT_ANODE <= "1111";
+                    else
+                        DIGIT_ANODE <= "1101";
+                        SEGMENT <= Seg_reg_1;
+
+                    end if;
+                when "10" =>
+                    if Seg_reg_2 = "00000000" then
+                        DIGIT_ANODE <= "1111";
+                    else
+                        DIGIT_ANODE <= "1011";
+                        SEGMENT <= Seg_reg_2;
+                    end if;
+                when "11" =>
+                    if Seg_reg_3 = "00000000" then
+                        DIGIT_ANODE <= "1111";
+                    else
+                        DIGIT_ANODE <= "0111";
+                        SEGMENT <= Seg_reg_3;
+                    end if;
+                when others =>
+                    DIGIT_ANODE <= "1111";
+            end case;
+        end if;
+    end process;
 end behavioral;
--- VHDL/binary2BCD.vhd
+++ VHDL/binary2BCD.vhd
@@ -19,25 +19,25 @@ end binary2BCD;
 
 architecture structural of binary2BCD is
  
- -- 4 registers, 1 (4bit)counter, 2 (8bit and 10bit)shift registers, 1 (8bit)buffer. Total: 30bits
+ -- 4 registers, 1 (4bit)counter, 1 (10bit) shift register,1 (10bit) buffer register, 1 (8bit)buffer. Total: 32bits
  
-    signal Binary_in_buff: std_logic_vector(WIDTH-1 downto 0);  
+    signal Binary_in_buff: std_logic_vector(WIDTH-1 downto 0);   -- binary_in buffer
     signal Binary_in_buff_next: std_logic_vector(WIDTH-1 downto 0); 
 
-    signal  BCD_sreg: std_logic_vector (9 downto 0);
+    signal  BCD_sreg: std_logic_vector (9 downto 0); -- Shift register for Double-Dabble
     signal  BCD_sreg_next: std_logic_vector (9 downto 0);
 
-    signal  counter: std_logic_vector (3 downto 0);
+    signal  counter: std_logic_vector (3 downto 0);  -- 8 bit counter
     signal  counter_next: std_logic_vector (3 downto 0);
-    signal  count_done: std_logic;
+    signal  count_done: std_logic; --Count complete signal
 
-    signal  BCD_out_buffer: std_logic_vector (9 downto 0);
+    signal  BCD_out_buffer: std_logic_vector (9 downto 0);  --output buffer, refreshes on count_done
     signal  BCD_out_buffer_next: std_logic_vector (9 downto 0);
     
     
 begin
 
-    Double_dabble_sequential: process(clk,counter) --converts bin to bcd, BCD_sreg is the output signal
+    Double_dabble_sequential: process(clk,counter) --converts bin to bcd, uses the Double dabble algorythm
     begin
         if(rising_edge(clk)) then
             if reset='1' then             
@@ -50,27 +50,27 @@ begin
         end if;      
     end process;
 
-    Double_dabble_combinational: process(reset,binary_in,counter,Binary_in_buff,BCD_sreg)--converts bin to bcd, BCD_sreg is the output signal
+    Double_dabble_combinational: process(reset,binary_in,Binary_in_buff,BCD_sreg,count_done)--converts bin to bcd, uses the Double dabble algorythm
     begin
         if reset='1' then  
             BCD_sreg_next<="0000000000";
             Binary_in_buff_next<=binary_in;
         else
             if(count_done='0') then
-                Binary_in_buff_next(WIDTH-1 downto 1)<=Binary_in_buff(WIDTH-2 downto 0);
-                Binary_in_buff_next(0)<='0';
-               if(BCD_sreg(3 downto 0)>="0101") then
-                        BCD_sreg_next(9 downto 1)<=BCD_sreg(8 downto 0)+"0011";
+                Binary_in_buff_next(WIDTH-1 downto 1)<=Binary_in_buff(WIDTH-2 downto 0); --shift bit from binary_in buffer to BCD_shift register
+                Binary_in_buff_next(0)<='0'; 
+               if(BCD_sreg(3 downto 0)>="0101") then -- if the low nibble of the shift register is larger than 5, add 3 to it, and left shift 1
+                        BCD_sreg_next(9 downto 1)<=BCD_sreg(8 downto 0)+"0011"; 
                         BCD_sreg_next(0)<=Binary_in_buff(WIDTH-1);
-              elsif (BCD_sreg(7 downto 4)>="0101") then
+              elsif (BCD_sreg(7 downto 4)>="0101") then  -- if the high nibble of the shift register is larger than 5, add 3 to it, and left shift 1
                         BCD_sreg_next(9 downto 1)<=BCD_sreg(8 downto 0)+"00110000";
                         BCD_sreg_next(0)<=Binary_in_buff(WIDTH-1);
                else
-                        BCD_sreg_next(9 downto 1)<=BCD_sreg(8 downto 0);
+                        BCD_sreg_next(9 downto 1)<=BCD_sreg(8 downto 0); -- else, just left shift 1
                         BCD_sreg_next(0)<=Binary_in_buff(WIDTH-1);
                end if;       
             else               
-                BCD_sreg_next<="0000000000";     
+                BCD_sreg_next<="0000000000";   -- when all 8 bits have been shifted, reset shift register, buffer the input and start again
                 Binary_in_buff_next<=binary_in;                
            end if;
         end if;
@@ -78,7 +78,7 @@ begin
 
 
 
-Output_buffer_sequential: process(clk,reset,BCD_out_buffer_next)
+Output_buffer_sequential: process(clk,reset,BCD_out_buffer_next)  -- output buffer, used to store the output signal until the next conversion is ready
 begin
      if(rising_edge(clk)) then
        if reset='1' then
@@ -89,21 +89,20 @@ begin
    end if;      
 end process;
 
-Output_buffer_combinational: process(reset,count_done,BCD_out_buffer)
+Output_buffer_combinational: process(reset,count_done,BCD_out_buffer) -- output buffer, used to store the output signal until the next conversion is ready
    begin   
           if reset='1' then
              BCD_out_buffer_next<="0000000000";
           elsif(count_done='1') then 
-             
-             BCD_out_buffer_next<=BCD_sreg;
+             BCD_out_buffer_next<=BCD_sreg; -- if the conversion is complete, refresh the output buffer
            else
-            BCD_out_buffer_next<= BCD_out_buffer;        
+            BCD_out_buffer_next<= BCD_out_buffer;   --else, keep the same value    
           end if;
           BCD_out<=BCD_out_buffer; 
 end process;
 
 
-Counter_sequential: process(clk,reset,counter_next)
+Counter_sequential: process(clk,reset,counter_next) --counter register, used to count the conversion's steps, counts up to 9
  begin   
          if(rising_edge(clk)) then
        if reset='1' then
@@ -114,16 +113,16 @@ Counter_sequential: process(clk,reset,counter_next)
    end if; 
 end process;
 
-Counter_combinational: process(clk,reset,counter)
+Counter_combinational: process(clk,reset,counter)--counter register, used to count the conversion's steps, counts up to 9
  begin   
         if reset='1' then
            counter_next<="0000";
            count_done<='1'; 
-         elsif(counter<="1000") then 
+         elsif(counter<="1000") then  
             counter_next<=counter+1;
             count_done<='0';
           else
-           counter_next<="0000";
+           counter_next<="0000"; --if the coutner reaches 9, zero it and start again
            count_done<='1';        
          end if;
          BCD_out<=BCD_out_buffer; 
